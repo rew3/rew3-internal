03545a55b1dd85e10963fda949b063f0c36a5b4a
Srijesh Khanal <khanal.srijesh@rew3.com>
2023-05-11 21:33:58 +0545

    Added Base Struct for CRM

---
+++ model/PaginatedResult.go
@@ -0,0 +1,38 @@
+package model
+
+import (
+	"encoding/json"
+	"math"
+)
+
+/*
+This class provides pagination information to the query results
+@param data data response of the query
+@param limit number of entities returned
+@param pageNumber pageNumber of the repsonse
+@param pagesCount total pagesCount
+@param totalCount total number of entites
+@author rew3 on 2023/05/11.
+@version 1.0.0
+*/
+type PaginatedResult struct {
+	Data       []json.RawMessage `bson:"data"`
+	Limit      int               `bson:"limit"`
+	PageNumber int               `bson:"page_number"`
+	PagesCount int               `bson:"pages_count"`
+	TotalCount int               `bson:"total_count"`
+}
+
+func (pr *PaginatedResult) CountPages() *PaginatedResult {
+	pageCount := 0
+	if pr.Limit > 0 {
+		pageCount = int(math.Ceil(float64(pr.TotalCount) / float64(pr.Limit)))
+	}
+	return &PaginatedResult{
+		Data:       pr.Data,
+		Limit:      pr.Limit,
+		PageNumber: pr.PageNumber,
+		PagesCount: pageCount,
+		TotalCount: pr.TotalCount,
+	}
+}
---
+++ model/RequestParam.go
@@ -0,0 +1,22 @@
+package model
+
+import "encoding/json"
+
+type RequestParam struct {
+	Limit         *int                `bson:"limit,omitempty"`
+	Expand        *bool               `bson:"expand,omitempty"`
+	LimitFields   *string             `bson:"limit_fields,omitempty"`
+	Offset        *int                `bson:"offset,omitempty"`
+	Sort          *json.RawMessage    `bson:"sort,omitempty"`    //We can use *map[string]interface{} as well
+	Filters       *json.RawMessage    `bson:"filters,omitempty"` //*map[string]interface{}
+	RawParameters map[string][]string //This is not saved in DB. Rather used in following methods
+}
+
+func (r *RequestParam) WithRawParameters(params map[string][]string) *RequestParam {
+	r.RawParameters = params
+	return r
+}
+
+func (r *RequestParam) GetRawParameters() map[string][]string {
+	return r.RawParameters
+}
