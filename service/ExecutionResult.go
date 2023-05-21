9907820ff73fafc042ed0425db4194b8bc3c0db3
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2023-05-21 21:26:08 +0545

    Added ExecutionResult model

---
+++ service/ExecutionResult.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"github.com/rew3/rew3-base/service/constants"
+)
+
+type ExecutionResult struct {
+	IsSuccessful bool
+	Status       constants.StatusType
+	Message      string
+	Id           string
+	Action       string
+	Data         interface{}
+}
