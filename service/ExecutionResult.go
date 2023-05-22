21fc3218aadcc278b7e1f3506c26ebe67260aa77
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2023-05-22 13:34:41 +0545

    Added generic field

--- service/ExecutionResult.go
+++ service/ExecutionResult.go
@@ -4,11 +4,11 @@ import (
 	"github.com/rew3/rew3-base/service/constants"
 )
 
-type ExecutionResult struct {
+type ExecutionResult[T any] struct {
 	IsSuccessful bool
 	Status       constants.StatusType
 	Message      string
 	Id           string
 	Action       string
-	Data         interface{}
+	Data         T
 }
