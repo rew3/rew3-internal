615c4f92e91da484b3387a709d12b27b02bf12d5
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2023-06-02 19:42:53 +0545

    Added execution result methods

--- service/response/ExecutionResult.go
+++ service/response/ExecutionResult.go
@@ -12,3 +12,56 @@ type ExecutionResult[T any] struct {
 	Action       string
 	Data         T
 }
+
+func ErrorExecutionResult[T any](id string, action string, message string, status constants.StatusType) *ExecutionResult[T] {
+	return &ExecutionResult[T]{
+		IsSuccessful: false,
+		Status:       status,
+		Message:      message,
+		Id:           id,
+		Action:       action,
+	}
+}
+
+func SuccessExecutionResult[T any](id string, action string, message string, status constants.StatusType, data T) *ExecutionResult[T] {
+	return &ExecutionResult[T]{
+		IsSuccessful: true,
+		Status:       status,
+		Message:      message,
+		Id:           id,
+		Action:       action,
+		Data:         data,
+	}
+}
+
+func CreateExecutionResult[T any](id string, action string, message string, data T) *ExecutionResult[T] {
+	return &ExecutionResult[T]{
+		IsSuccessful: true,
+		Status:       constants.CREATED,
+		Message:      message,
+		Id:           id,
+		Action:       action,
+		Data:         data,
+	}
+}
+
+func UpdateExecutionResult[T any](id string, action string, message string, data T) *ExecutionResult[T] {
+	return &ExecutionResult[T]{
+		IsSuccessful: true,
+		Status:       constants.OK,
+		Message:      message,
+		Id:           id,
+		Action:       action,
+		Data:         data,
+	}
+}
+
+func DeleteExecutionResult[T any](id string, action string, message string) *ExecutionResult[T] {
+	return &ExecutionResult[T]{
+		IsSuccessful: true,
+		Status:       constants.DELETED,
+		Message:      message,
+		Id:           id,
+		Action:       action,
+	}
+}
