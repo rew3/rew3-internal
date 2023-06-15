e56f597f45a853d5b7b252bc0cd78a4199522885
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2023-06-15 20:04:42 +0545

    Added data mapper utility

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.20
 
 require (
 	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/mitchellh/mapstructure v1.5.0
+	github.com/segmentio/encoding v0.3.6
 	go.mongodb.org/mongo-driver v1.11.6
+	google.golang.org/protobuf v1.30.0
 )
 
 require (
@@ -15,12 +18,14 @@ require (
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/segmentio/asm v1.1.3 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.1 // indirect
 	github.com/xdg-go/stringprep v1.0.3 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
 	golang.org/x/text v0.3.8 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
---
+++ pkg/utils/mapper/data_mapper.go
@@ -0,0 +1,161 @@
+package mapper
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/segmentio/encoding/json"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
+)
+
+/*
+ * Convert Model type to Proto Type.
+ * Note: Proto Message type P must be pointer type i.e. *ProtoMessage e.g. *ContactProto.
+ */
+func MapModelToPB[M any, P proto.Message](model M, defaultValue P) (P, error) {
+	// Converting model to Map.
+	maps, err := buildMapFromModel(model)
+	if err != nil {
+		fmt.Printf("MapModelToProto: Error while converting from Modal to Maps: %v", err)
+		return defaultValue, err
+	}
+	// Converting maps to proto.
+	proto, err := buildProtoFromMap(maps, defaultValue)
+	if err != nil {
+		fmt.Printf("MapModelToProto: Error while converting from Maps to Proto: %v", err)
+		return defaultValue, err
+	}
+	return proto, nil
+}
+
+/*
+ * Convert Proto type to Model Type.
+ * If trimString provided true, all string field with leading/trailing whitespaces will be trimed.
+ * Note: Proto Message type P must be pointer type i.e. *ProtoMessage e.g. *ContactProto.
+ */
+func MapModelFromPB[M any, P proto.Message](proto P, defaultValue M, trimString bool) (M, error) {
+	// Converting proto to Map.
+	maps, err := buildMapFromProto(proto)
+	if err != nil {
+		fmt.Printf("MapModelFromProto: Error while converting from Proto to Maps: %v", err)
+		return defaultValue, err
+	}
+	if(trimString) {
+		recursivelyTrimStrings(maps)
+	}
+	// Converting maps to Model.
+	result, err := buildModelFromMap(maps, &defaultValue)
+	if err != nil {
+		fmt.Printf("MapModelFromProto: Error while converting from Maps to Model: %v", err)
+		return defaultValue, err
+	}
+	return *result, nil
+}
+
+// recursivelyTrimStrings trims string values in a nested map structure
+func recursivelyTrimStrings(data interface{}) {
+	switch d := data.(type) {
+	case map[string]interface{}:
+		for key, value := range d {
+			if strValue, ok := value.(string); ok {
+				trimmedValue := strings.TrimSpace(strValue)
+				d[key] = trimmedValue
+			} else {
+				recursivelyTrimStrings(value)
+			}
+		}
+	case []interface{}:
+		for i, v := range d {
+			recursivelyTrimStrings(v)
+			d[i] = v
+		}
+	}
+}
+
+/*
+ * INTERNAL: Convert Proto type to Map.
+ */
+func buildMapFromProto(pbMessage proto.Message) (map[string]interface{}, error) {
+	// Create a new JSON marshaller
+	marshaler := protojson.MarshalOptions{
+		UseProtoNames:   true,
+		EmitUnpopulated: false,
+	}
+	// Marshal the Protobuf message to JSON
+	jsonBytes, err := marshaler.Marshal(pbMessage)
+	if err != nil {
+		return nil, err
+	}
+	// Unmarshal the JSON to a map
+	var resultMap map[string]interface{}
+	strings.TrimSpace(string(jsonBytes))
+	if err = json.Unmarshal(jsonBytes, &resultMap); err != nil {
+		return nil, err
+	}
+	return resultMap, nil
+}
+
+/*
+ * INTERNAL: Convert Map to Proto type.
+ *
+ * Param:
+ * inputMap - to convert values in map.
+ * pbStruct - Values will be read to this proto struct.
+ */
+func buildProtoFromMap[T proto.Message](inputMap map[string]interface{}, pbStruct T) (T, error) {
+	// Marshal the input map to JSON
+	jsonData, err := json.Marshal(inputMap)
+	if err != nil {
+		return pbStruct, err
+	}
+	// Unmarshal the JSON data into the Protobuf struct
+	if err := protojson.Unmarshal(jsonData, pbStruct); err != nil {
+		return pbStruct, err
+	}
+	return pbStruct, nil
+}
+
+/*
+ * INTERNAL: Convert Model type to Map.
+ */
+func buildMapFromModel(model interface{}) (map[string]interface{}, error) {
+	var resultMap map[string]interface{}
+	jsonData, err := json.Marshal(model)
+	if err != nil {
+		return resultMap, err
+	}
+	if err := json.Unmarshal(jsonData, &resultMap); err != nil {
+		return resultMap, err
+	}
+	return resultMap, nil
+}
+
+/*
+ * INTERNAL: Convert Map to Model type.
+ * Note: this will automatically convert RFC3339 format string date to time.Time. Any invalid string
+ * date value will not be able to parse and throw error.
+ *
+ * Param:
+ * inputMap - to convert values in map.
+ * model - pointer to the struct that will contain the decoded value.
+ */
+func buildModelFromMap[T any](inputMap map[string]interface{}, model T) (T, error) {
+	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+		TagName: "json",
+		Result:  model,
+		Squash:  true,
+		DecodeHook: mapstructure.ComposeDecodeHookFunc(
+			mapstructure.StringToTimeHookFunc(time.RFC3339), // Convert string to time.Time using RFC3339 format
+		),
+	})
+	if err != nil {
+		return model, err
+	}
+	if err := decoder.Decode(inputMap); err != nil {
+		return model, err
+	}
+	return model, nil
+}
