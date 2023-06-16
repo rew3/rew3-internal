5fbad6ce58c3c1a612f70cf8a4f787eb9eaaeaef
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2023-06-16 12:38:14 +0545

    git checkout main

--- service/command/command_controller.go
+++ service/command/command_controller.go
@@ -3,5 +3,5 @@ package command
 import "context"
 
 type CommandController interface {
-	Dispatch(ctx context.Context, command Command, resultChannel CommandResultChannel)
+	Dispatch(ctx context.Context, command Command, resultChannel *CommandResultChannel)
 }
