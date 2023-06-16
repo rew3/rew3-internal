7466ab6db10ffe8043f539aee6d2d77870597e6b
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2023-06-16 12:39:18 +0545

    Changes in command result and channel, mongodb client

--- db/mongo/mongo_client.go
+++ db/mongo/mongo_client.go
@@ -3,14 +3,17 @@ package mongo
 import (
 	"context"
 	"log"
+	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoClient struct {
-	db     *mongo.Database
-	client *mongo.Client
+	db            *mongo.Database
+	dbName        string
+	client        *mongo.Client
+	clientOptions *options.ClientOptions
 }
 
 /**
@@ -18,20 +21,42 @@ type MongoClient struct {
 * It is advices to use single instance of MongoClient per application.
  */
 func NewMongoClient(mongoURL string, mongoDatabase string) *MongoClient {
-	// Set up MongoDB connection
 	clientOptions := options.Client().ApplyURI(mongoURL)
-	client, err := mongo.Connect(context.Background(), clientOptions)
+	client := MongoClient{dbName: mongoDatabase, clientOptions: clientOptions}
+	client.connect()
+	return &client
+}
+
+/**
+ * Connect to mongodb client.
+ */
+func (client *MongoClient) connect() {
+	// Set up MongoDB connection
+	// Set a timeout for the initial connection
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	mongoClient, err := mongo.Connect(ctx, client.clientOptions)
 	if err != nil {
 		log.Fatalf("Failed to connect to MongoDB: %v", err)
 	}
-	db := client.Database(mongoDatabase)
-	return &MongoClient{db: db, client: client}
+	if err := mongoClient.Ping(context.Background(), nil); err != nil {
+		// Connection is not active or there was an error
+		log.Println("ERROR: MongoDB Client connection is not connected.")
+	} else {
+		log.Println("SUCCESS: MongoDB Client is connected.")
+	}
+	client.db = mongoClient.Database(client.dbName)
+	client.client = mongoClient
 }
 
 /**
 * Get MongoDB Collection instance for given collection name.
  */
 func (client *MongoClient) GetCollection(collectionName string) *mongo.Collection {
+	if client.client == nil || client.db == nil {
+		log.Println("ERROR: MongoDB Client unavailable. Connecting...")
+		client.connect()
+	}
 	return client.db.Collection(collectionName)
 }
 
