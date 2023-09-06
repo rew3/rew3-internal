26f21437ee2d2b78835a0454586675a63b5b8df2
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2023-09-06 19:45:49 +0545

    changed to data type

--- app/common/Reference.go
+++ app/common/Reference.go
@@ -1,15 +1,13 @@
 package common
 
 import (
-	"encoding/json"
-
 	c "github.com/rew3/rew3-internal/app/common/constants"
 )
 
 type Reference struct {
-	Data     json.RawMessage `json:"data,omitempty" bson:"data,omitempty"`
-	Module   c.Module        `json:"module" bson:"module"`
-	Entity   c.Entity        `json:"entity,omitempty" bson:"entity,omitempty"`
-	EntityID string          `json:"entity_id,omitempty" bson:"entity_id,omitempty"`
-	Title    string          `json:"title,omitempty" bson:"title,omitempty"`
+	Data     interface{} `json:"data,omitempty" bson:"data,omitempty"`
+	Module   c.Module    `json:"module" bson:"module"`
+	Entity   c.Entity    `json:"entity,omitempty" bson:"entity,omitempty"`
+	EntityID string      `json:"entity_id,omitempty" bson:"entity_id,omitempty"`
+	Title    string      `json:"title,omitempty" bson:"title,omitempty"`
 }
