a2223b04b71986d3559d21f08e1a4e58a753c611
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2023-09-06 23:28:22 +0545

    change to string type

--- app/common/Reference.go
+++ app/common/Reference.go
@@ -5,9 +5,9 @@ import (
 )
 
 type Reference struct {
-	Data     interface{} `json:"data,omitempty" bson:"data,omitempty"`
-	Module   c.Module    `json:"module" bson:"module"`
-	Entity   c.Entity    `json:"entity,omitempty" bson:"entity,omitempty"`
-	EntityID string      `json:"entity_id,omitempty" bson:"entity_id,omitempty"`
-	Title    string      `json:"title,omitempty" bson:"title,omitempty"`
+	Data     string   `json:"data,omitempty" bson:"data,omitempty"`
+	Module   c.Module `json:"module" bson:"module"`
+	Entity   c.Entity `json:"entity,omitempty" bson:"entity,omitempty"`
+	EntityID string   `json:"entity_id,omitempty" bson:"entity_id,omitempty"`
+	Title    string   `json:"title,omitempty" bson:"title,omitempty"`
 }
