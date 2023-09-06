4b88b703dde77770833deb018179c834b8aeab65
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2023-09-06 23:49:45 +0545

    removed data field

--- app/common/Reference.go
+++ app/common/Reference.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Reference struct {
-	Data     string   `json:"data,omitempty" bson:"data,omitempty"`
+	//Data     string   `json:"data,omitempty" bson:"data,omitempty"` // TODO - removed, confirm its usage later.
 	Module   c.Module `json:"module" bson:"module"`
 	Entity   c.Entity `json:"entity,omitempty" bson:"entity,omitempty"`
 	EntityID string   `json:"entity_id,omitempty" bson:"entity_id,omitempty"`
