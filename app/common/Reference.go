c95cdb30180d7079eb1ab2a5ba8f9f07c6c83d67
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2023-09-29 02:49:40 +0545

    made module omitempty

--- app/common/Reference.go
+++ app/common/Reference.go
@@ -6,7 +6,7 @@ import (
 
 type Reference struct {
 	//Data     string   `json:"data,omitempty" bson:"data,omitempty"` // TODO - removed, confirm its usage later.
-	Module   c.Module `json:"module" bson:"module"`
+	Module   c.Module `json:"module,omitempty" bson:"module,omitempty"`
 	Entity   c.Entity `json:"entity,omitempty" bson:"entity,omitempty"`
 	EntityID string   `json:"entity_id,omitempty" bson:"entity_id,omitempty"`
 	Title    string   `json:"title,omitempty" bson:"title,omitempty"`
