3f0abe877867d3e2e035edc3353cec8fe79b2f23
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2023-10-20 22:59:40 +0545

    seperated command and query methods in service mapping

--- service/grpc/server/service_mapping.go
+++ service/grpc/server/service_mapping.go
@@ -18,20 +18,18 @@ import (
  * This utility maps the requried API to its respective command/query executor execution and register to GRPC Service method registry.
  */
 
-type ExecutorType string
-
-const (
-	COMMAND ExecutorType = "Command"
-	QUERY   ExecutorType = "Query"
-)
-
 type MappingContext struct {
-	registry        *ServiceMethodRegistry
+	Registry        *ServiceMethodRegistry
 	CommandExecutor *executor.CommandExecutor
 	QueryExecutor   *executor.QueryExecutor
 }
 
-func MapAPI[Input any, Output any](bc *MappingContext, api api.APIOperation, exType ExecutorType, output Output, payloadResponseType grpc.DataType) {
+/*
+ * Map given API to its respective command executor.
+ * Input - type for payload data to parse as input i.e. Command. Note: payload must be valid structure as its command.
+ * Output - output response type returned by command handler.
+ */
+func MapCmdAPI[Input any, Output any](bc *MappingContext, api api.APIOperation, responseMeta grpc.DataType) {
 	method := NewServiceMethod(api).BindHandler(
 		func(ctx context.Context, rc request.RequestContext, input *grpc.PayloadWrapper) *grpc.ResponsePayload {
 			parsed, err := grpc.ParseFullPayload[Input](input)
@@ -39,16 +37,33 @@ func MapAPI[Input any, Output any](bc *MappingContext, api api.APIOperation, exT
 				return grpc.InvalidRequestResponsePayload(api, "Invalid request data provided: "+err.Error())
 			}
 			updatedCtx := ctxUtil.WithRequestContext(ctx, rc)
-			if exType == COMMAND {
-				cmdResult := bc.CommandExecutor.Execute(updatedCtx, parsed)
-				er := baseCommand.ParseCommandResult[Output](&cmdResult, output)
-				return grpc.ToResponsePayload(api, er.Status, er.Message, er.Data, payloadResponseType)
-			} else {
-				queryResult := bc.QueryExecutor.Execute(updatedCtx, parsed)
-				er := baseQuery.ParseQueryResult[Output](&queryResult, output)
-				return grpc.ToResponsePayload(api, er.Status, er.Message, er.Data, payloadResponseType)
-			}
+			var output Output
+			cmdResult := bc.CommandExecutor.Execute(updatedCtx, parsed)
+			er := baseCommand.ParseCommandResult[Output](&cmdResult, output)
+			return grpc.ToResponsePayload(api, er.Status, er.Message, er.Data, responseMeta)
 		},
 	)
-	bc.registry.AddServiceMethod(method)
+	bc.Registry.AddServiceMethod(method)
+}
+
+/*
+ * Map given API to its respective query executor.
+ * Input - type for payload data to parse as input i.e. Query. Note: payload must be valid structure as its query.
+ * Output - output response type returned by query handler.
+ */
+func MapQueryAPI[Input any, Output any](bc *MappingContext, api api.APIOperation, responseMeta grpc.DataType) {
+	method := NewServiceMethod(api).BindHandler(
+		func(ctx context.Context, rc request.RequestContext, input *grpc.PayloadWrapper) *grpc.ResponsePayload {
+			parsed, err := grpc.ParseFullPayload[Input](input)
+			if err != nil {
+				return grpc.InvalidRequestResponsePayload(api, "Invalid request data provided: "+err.Error())
+			}
+			updatedCtx := ctxUtil.WithRequestContext(ctx, rc)
+			var output Output
+			queryResult := bc.QueryExecutor.Execute(updatedCtx, parsed)
+			er := baseQuery.ParseQueryResult[Output](&queryResult, output)
+			return grpc.ToResponsePayload(api, er.Status, er.Message, er.Data, responseMeta)
+		},
+	)
+	bc.Registry.AddServiceMethod(method)
 }
