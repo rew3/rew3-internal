fb5b45b89290d3955ed8b99e3730b2846277f289
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2024-01-04 02:32:52 +0545

    minor changes

--- pubsub/mq/mq_client.go
+++ pubsub/mq/mq_client.go
@@ -48,6 +48,6 @@ func (s *MQClient) Consumer(props config.ConsumerProps) types.Consumer {
 /**
  * Create new consumer group.
  */
-func (s *MQClient) ConsumerGroup(props config.ConsumerProps) types.ConsumerGroup {
+func (s *MQClient) ConsumerGroup() types.ConsumerGroup {
 	return rabbitmq.NewConsumerGroup(s.connection)
 }
