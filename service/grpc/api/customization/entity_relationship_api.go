00b8a219d8ac3a116e841d5e0d2057f8db588efe
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2024-02-16 00:54:20 +0545

    renamed api for relationship

--- service/grpc/api/customization/entity_relationship_api.go
+++ service/grpc/api/customization/entity_relationship_api.go
@@ -11,6 +11,5 @@ const (
 	UNLINK_ENTITY api.APIOperation = "erm_unlinkEntity"
 
 	// READ APIs.
-	LIST_RELATED_ENTITIES  api.APIOperation = "erm_listRelatedEntities"
-	COUNT_RELATED_ENTITIES api.APIOperation = "erm_countRelatedEntities"
+	LIST_LINKED_ENTITIES  api.APIOperation = "erm_listLinkedEntities"
 )
