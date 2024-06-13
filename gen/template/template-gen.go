4c28666068e04872255006c40d491d8a0cab074a
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2024-06-14 03:37:18 +0545

    fixed issue with template generator

--- gen/template/template-gen.go
+++ gen/template/template-gen.go
@@ -3,6 +3,7 @@ package template
 import (
 	"log"
 	"os"
+	"path"
 	"text/template"
 	"time"
 
@@ -24,32 +25,35 @@ type TemplateConfig struct {
  * Generate code/file from template with given template configuration.
  */
 func GenerateFromTemplate(config TemplateConfig) error {
-	tmpl, err := template.ParseFiles(config.TemplatePath)
-	if err != nil {
-		log.Fatal("Error parsing template:", err)
-	}
-	tmpl = tmpl.Funcs(template.FuncMap{
-		"CurrentDate": func() string {
+	fn := template.FuncMap{
+		"currentDate": func() string {
 			return time.Now().Format("2006-01-02")
 		},
-		"Version": func() string {
+		"version": func() string {
 			return config.Version
 		},
-	})
+	}
+	files := []string{config.TemplatePath}
+	tName := path.Base(config.TemplatePath)
+	tmpl := template.Must(template.New(tName).Funcs(fn).ParseFiles(files...))
+	/*if err != nil {
+		log.Fatal("Error parsing template:", err)
+		return err
+	}*/
 	if config.DeleteIfExist {
 		os.Remove(config.OutputPath)
 	} else if fUtil.IsFileAlreadyExists(config.OutputPath) {
-		log.Fatal("Unable to generate file. Output file already exists:", err)
+		log.Fatal("Unable to generate file. Output file already exists:")
 	}
 	outputFile, err := fUtil.CreateFile(config.OutputPath)
 	if err != nil {
 		log.Fatal("Error creating output file:", err)
 	}
-	defer outputFile.Close()
 	err = tmpl.Execute(outputFile, config.Data)
 	if err != nil {
 		log.Fatal("Error executing template:", err)
 	}
+	defer outputFile.Close()
 	log.Printf("Template generation completed. Output file: %v", outputFile)
 	return nil
 }
