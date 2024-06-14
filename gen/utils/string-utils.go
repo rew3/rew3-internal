1e14fa1bb47c488ef9cf323c04ef633d41f36e9f
Ranjit-Rew3 <ranjit.kaliraj@rew3.com>
2024-06-15 02:40:44 +0545

    fixed issue with CleanDirPath method

--- gen/utils/string-utils.go
+++ gen/utils/string-utils.go
@@ -21,8 +21,17 @@ func CapitalizeFirst(s string) string {
  * Removes redundant slashes from the given path.
  * Note: it will also remove leading and trailing slashes.
  * foo//bar/cat, to foo/bar/cat.
+ * Note: it will not touch leading or trailing /.
  */
 func CleanDirPath(path string) string {
+	if path == "" {
+		return path
+	}
+
+	// Check for leading and trailing slashes
+	hasLeadingSlash := strings.HasPrefix(path, "/")
+	hasTrailingSlash := strings.HasSuffix(path, "/")
+
 	// Split the path into segments
 	segments := strings.Split(path, "/")
 
@@ -35,5 +44,17 @@ func CleanDirPath(path string) string {
 	}
 
 	// Join the cleaned segments with a single slash
-	return strings.Join(cleanedSegments, "/")
+	cleanedPath := strings.Join(cleanedSegments, "/")
+
+	// Add back the leading slash if it was present
+	if hasLeadingSlash {
+		cleanedPath = "/" + cleanedPath
+	}
+
+	// Add back the trailing slash if it was present
+	if hasTrailingSlash && cleanedPath != "" {
+		cleanedPath = cleanedPath + "/"
+	}
+
+	return cleanedPath
 }
